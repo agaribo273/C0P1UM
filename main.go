a20496304cef1272007fea56ae2a27fd91caff1a
Jovanny Ramos <jovannyr72@gmail.com>
2024-06-14 16:17:01 -0700

    Added boss command that gives random osrs boss to do

--- main.go
+++ main.go
@@ -3,13 +3,17 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
 )
+
+const prefix string = "!cope"
 
 func main() {
 
@@ -30,8 +34,51 @@ func main() {
 			return
 		}
 
-		if m.Content == "hello" || m.Content == "Hello" {
-			s.ChannelMessageSend(m.ChannelID, "world!")
+		args := strings.Split(m.Content, " ")
+
+		if args[0] != prefix {
+			return
+		}
+
+		if args[1] == "test" {
+			s.ChannelMessageSend(m.ChannelID, "initiated!")
+		}
+
+		if args[1] == "boss" {
+			proverbs := []string{
+				"Do Vorkath",
+				"Do Leviathan",
+				"Do Barrows",
+				"Run Tombs of Amascut",
+				"Run Chambers of Xeric",
+				"Run Theatre of Blood",
+				"Do Giant Mole",
+				"Do Dagannoth Kings",
+				"Do Bandos",
+				"Do Zulrah",
+				"Do Muspah",
+				"Do Duke Sucellus",
+				"Do Vardorvis",
+				"Do Corrupted Gauntlet",
+				"Run Colosseum",
+				"Run Inferno",
+				"Do slayer instead loser",
+			}
+
+			selection := rand.Intn(len(proverbs))
+
+			// ### This section of code is if we want to make an embedded message ###
+			// author := discordgo.MessageEmbedAuthor{
+			// 	Name: "That dude Jeff",
+			// 	URL:  "https://github.com/ItsJeff510",
+			// }
+
+			// embed := discordgo.MessageEmbed{
+			// 	Title:  proverbs[selection],
+			// 	Author: &author,
+			// }
+
+			s.ChannelMessageSend(m.ChannelID, proverbs[selection])
 		}
 	})
 
