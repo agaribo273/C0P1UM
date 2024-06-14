731050d3ca2c44bba35ba7f48c9587d5c3c5fb68
Jovanny Ramos <jovannyr72@gmail.com>
2024-06-13 20:20:30 -0700

    Discord Connection Established

--- main.go
+++ main.go
@@ -1,2 +1,49 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func main() {
+
+	secret, err := os.ReadFile("botID.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sess, err := discordgo.New(string(secret))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sess.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		if m.Author.ID == s.State.User.ID {
+			return
+		}
+
+		if m.Content == "hello" || m.Content == "Hello" {
+			s.ChannelMessageSend(m.ChannelID, "world!")
+		}
+	})
+
+	sess.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+
+	err = sess.Open()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer sess.Close()
+
+	fmt.Println("The bot is online!")
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+}
